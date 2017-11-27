26d2321dd1735b996e877642c15c0ed47af25d17
hellobling <623088099@qq.com>
2017-11-27 22:28:02 +0800

    base

--- src/App.vue
+++ src/App.vue
@@ -18,38 +18,6 @@
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  import { urlParse } from 'common/js/util';
-  import header from 'components/header/header.vue';
-
-  const ERR_OK = 0;
-  const debug = process.env.NODE_ENV !== 'production';
-
-  export default {
-    data() {
-      return {
-        seller: {
-          id: (() => {
-            let queryParam = urlParse();
-            return queryParam.id;
-          })()
-        }
-      };
-    },
-    created() {
-      const url = debug ? '/api/seller' : 'http://ustbhuangyi.com/sell/api/seller';
-      this.$http.get(url + '?id=' + this.seller.id).then((response) => {
-        response = response.body;
-        if (response.errno === ERR_OK) {
-          this.seller = Object.assign({}, this.seller, response.data);
-        }
-      });
-    },
-    components: {
-      'v-header': header
-    }
-  };
-
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
--- src/main.js
+++ src/main.js
@@ -1,29 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
 import App from './App';
-import goods from 'components/goods/goods';
-import ratings from 'components/ratings/ratings';
-import seller from 'components/seller/seller';
-
-import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
-Vue.use(VueResource);
 
-const routes = [{
-  path: '/',
-  redirect: '/goods'
-}, {
-  path: '/goods',
-  component: goods
-}, {
-  path: '/ratings',
-  component: ratings
-}, {
-  path: '/seller',
-  component: seller
-}];
+const routes = [{}];
 
 const router = new VueRouter({
   linkActiveClass: 'active',

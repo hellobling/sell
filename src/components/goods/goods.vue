e549b411078e07f4a572e2918b42d8dfeb61d0a2
hellobling <623088099@qq.com>
2017-12-29 15:07:39 +0800

    add rating component

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,44 +1,48 @@
 <template>
-	<div class="goods">
-		<div class="menu-wrapper" ref="menuWrapper">
-			<ul>
-				<li v-for="(item,index) in goods" class="menu-item"  :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)">
-					<span class="text border-1px">
-						<span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
-					</span>
-				</li>
-			</ul>
+	<div>
+		<div class="goods">
+			<div class="menu-wrapper" ref="menuWrapper">
+				<ul>
+					<li v-for="(item,index) in goods" class="menu-item"  :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)">
+						<span class="text border-1px">
+							<span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+						</span>
+					</li>
+				</ul>
+			</div>
+			<div class="foods-wrapper" ref="foodsWrapper">
+				<ul>
+					<li v-for="(item,index) in goods" class="food-list" ref="foodList">
+						<h1 class="title">{{item.name}}</h1>
+						<ul>
+							<li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item border-1px">
+								<div class="icon">
+									<img width="57" height="57" :src="food.icon">
+								</div>
+								<div class="content">
+									<h2 class="name">{{food.name}}</h2>
+									<p class="desc">{{food.description}}</p>
+									<div class="extra">
+										<span class="count">月售{{food.sellCount}}份</span>
+										<span>好评率{{food.rating}}%</span>
+									</div>
+									<div class="price">
+										<span class="now">¥{{food.price}}</span>
+										<span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+									</div>
+									<div class="cartcontrol-wrapper">
+										<cartcontrol :food="food"></cartcontrol>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<shopcart :select-foods="selectFoods" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
 		</div>
-		<div class="foods-wrapper" ref="foodsWrapper">
-			<ul>
-				<li v-for="(item,index) in goods" class="food-list" ref="foodList">
-					<h1 class="title">{{item.name}}</h1>
-					<ul>
-						<li v-for="food in item.foods" class="food-item border-1px">
-							<div class="icon">
-								<img width="57" height="57" :src="food.icon">
-							</div>
-							<div class="content">
-								<h2 class="name">{{food.name}}</h2>
-								<p class="desc">{{food.description}}</p>
-								<div class="extra">
-									<span class="count">月售{{food.sellCount}}份</span>
-									<span>好评率{{food.rating}}%</span>
-								</div>
-								<div class="price">
-									<span class="now">¥{{food.price}}</span>
-									<span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
-								</div>
-								<div class="cartcontrol-wrapper">
-									<cartcontrol :food="food"></cartcontrol>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</div>
-		<shopcart :select-foods="selectFoods" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
+		
+		<food :food="selectedFood" ref="food"></food> 
 	</div>
 	
 </template>
@@ -46,6 +50,7 @@
 	import BScroll from 'better-scroll';
 	import shopcart from 'components/shopcart/shopcart';
 	import cartcontrol from 'components/cartcontrol/cartcontrol';
+	import food from 'components/food/food';
 	const ERR_OK = 0;
 	export default {
 		props: {
@@ -57,7 +62,8 @@
 			return {
 				goods: [],
 				listHeight: [],
-				scrollY: 0
+				scrollY: 0,
+				selectedFood: {}
 			};
 		},
 		computed: {
@@ -97,6 +103,13 @@
 			});
 		},
 		methods: {
+			selectFood(food, event) {
+				if (!event._constructed) {
+					return;
+				}
+				this.selectedFood = food;
+				this.$refs.food.show();
+			},
 			selectMenu(index, event) {
 				if (!event._constructed) {
 					return;
@@ -131,7 +144,8 @@
 		},
 		components: {
 			shopcart,
-			cartcontrol
+			cartcontrol,
+			food
 		}
 	};
 </script>
